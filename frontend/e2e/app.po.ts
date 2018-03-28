6ab4b5e902972912823d711ce086d778c912655d
Oleh <oleh.kosar94@gmail.com>
2018-03-29 01:13:07 +0300

    Update frontend part to Angular5 by ng cli

--- frontend/e2e/app.po.ts
+++ frontend/e2e/app.po.ts
@@ -1,6 +1,6 @@
-import { browser, element, by } from 'protractor';
+import { browser, by, element } from 'protractor';
 
-export class Ang2Page {
+export class AppPage {
   navigateTo() {
     return browser.get('/');
   }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,39 +1,18 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {HttpModule} from '@angular/http';
-import {RouterModule, Routes} from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import {AppComponent} from './app.component';
-import {PersonComponent} from './person/person.component';
-import {PersonListComponent} from './person-list/person-list.component';
-import {TableElementsCountComponent} from './table-elements-count/table-elements-count.component';
-import {TablePaginationComponent} from './table-pagination/table-pagination.component';
-import {TableSortComponent} from './table-sort/table-sort.component';
-import {PersonService} from './person.service'
 
-const appRoutes: Routes = [
-    {path: '', component: PersonListComponent},
-    {path: 'person/:id', component: PersonComponent, resolve: {person: PersonService}}
-];
+import { AppComponent } from './app.component';
+
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        PersonComponent,
-        PersonListComponent,
-        TableElementsCountComponent,
-        TablePaginationComponent,
-        TableSortComponent
-    ],
-    imports: [
-        BrowserModule,
-        FormsModule,
-        HttpModule,
-        RouterModule.forRoot(appRoutes)
-    ],
-    providers: [PersonService],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
