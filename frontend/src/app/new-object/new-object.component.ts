859ef38ca52c80528df8389c0c2ec4cab4ce0f82
Oleh <oleh.kosar94@gmail.com>
2018-04-02 14:21:51 +0300

    Step 3 of realty adding done

--- frontend/src/app/new-object/new-object.component.ts
+++ frontend/src/app/new-object/new-object.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from "@angular/core";
+import {ConfigService} from "../services/config.service";
 
 
 @Component({
@@ -7,9 +8,12 @@ import {Component, OnInit} from "@angular/core";
   styleUrls: ['./new-object.component.css']
 })
 export class NewObjectComponent implements OnInit {
-  public constructor() {
+  public supportedOperations: string[];
+
+  public constructor(public config: ConfigService) {
   }
 
   public ngOnInit() {
+    this.supportedOperations = this.config.getSupportedOperations();
   }
 }
--- frontend/src/app/services/config.service.ts
+++ frontend/src/app/services/config.service.ts
@@ -4,13 +4,17 @@ import {CityOnMap} from "../domain/domain";
 @Injectable()
 export class ConfigService {
   constructor() { }
-
   public userRegion: CityOnMap = {"name": "Львів", "lat": 49.8430008, "lng": 24.0215309};
+  public supportedOperations: string[] = ["SELLING", "RENT", "EXCHANGE"];
+
   public setUserRegion(value: any) {
     this.userRegion = value;
   }
-
   public getUserRegion(){
     return this.userRegion;
+  }
+
+  public getSupportedOperations(): string[] {
+    return this.supportedOperations;
   }
 }
