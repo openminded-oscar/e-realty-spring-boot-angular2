90ebe5c99637c0b35e74834bb9f5361fb7eee8ae
Oleh <>
2024-09-26 01:18:43 +0300

    Currently remove notifications service

--- frontend/src/app/realtor/realtors-gallery/realtors-gallery.component.ts
+++ frontend/src/app/realtor/realtors-gallery/realtors-gallery.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {RealterService} from '../../services/realter.service';
 import {Realter} from '../../domain/realter';
-import {NotificationsService} from 'angular2-notifications';
 import {Router} from '@angular/router';
 
 @Component({
@@ -13,8 +12,7 @@ export class RealtorsGalleryComponent implements OnInit {
   realters: Realter[];
 
   constructor(private realterService: RealterService,
-              private router: Router,
-              private _notification: NotificationsService) {
+              private router: Router) {
   }
 
   ngOnInit() {
