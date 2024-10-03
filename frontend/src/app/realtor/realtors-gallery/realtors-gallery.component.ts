08f38ea3e5628049b515a994c3bd0f1e741928bf
Oleh <>
2024-10-03 17:36:28 +0300

    Fix profilePic field for realtor

--- frontend/src/app/realtor/realtors-gallery/realtors-gallery.component.ts
+++ frontend/src/app/realtor/realtors-gallery/realtors-gallery.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {RealterService} from '../../services/realter.service';
 import {Realter} from '../../domain/realter';
 import {Router} from '@angular/router';
+import {Subject} from 'rxjs/Subject';
 
 @Component({
   selector: 'realtors-gallery',
   templateUrl: './realtors-gallery.component.html',
   styleUrls: ['./realtors-gallery.component.scss']
 })
-export class RealtorsGalleryComponent implements OnInit {
-  realters: Realter[];
+export class RealtorsGalleryComponent implements OnInit, OnDestroy {
+  private destroy$ = new Subject<boolean>();
+  public realters: Realter[];
+  public defaultRealtorPhoto = 'https://placehold.co/600x400?text=Realtor+photo';
 
   constructor(private realterService: RealterService,
               private router: Router) {
@@ -22,11 +25,21 @@ export class RealtorsGalleryComponent implements OnInit {
       });
   }
 
+  public setDefaultRealtorPhoto(event: Event) {
+    const imgElement = event.target as HTMLImageElement;
+    imgElement.src = this.defaultRealtorPhoto;
+  }
+
   goToRealtor(realter: Realter) {
     this.router.navigateByUrl(`/realtor/${realter.id}`);
   }
 
   addNewRealtor() {
     this.router.navigateByUrl(`/realtor`);
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
