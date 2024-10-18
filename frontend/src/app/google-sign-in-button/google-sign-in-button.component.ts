6ac64bbc3f12604a2a2fbaa03f36ca6103294387
Oleh <>
2024-10-19 01:11:42 +0300

    Google auth fix

--- frontend/src/app/google-sign-in-button/google-sign-in-button.component.ts
+++ frontend/src/app/google-sign-in-button/google-sign-in-button.component.ts
@@ -1,11 +1,7 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {SocialAuthService, SocialUser} from '@abacritt/angularx-social-login';
-import {takeUntil} from 'rxjs/operators';
-import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 import {Subject} from 'rxjs';
 import {UserService} from '../services/user.service';
-import {SigninSignoutService} from '../services/auth/signin-signout.service';
 
 @Component({
   selector: 'app-google-sign-in-button',
@@ -15,22 +11,10 @@ import {SigninSignoutService} from '../services/auth/signin-signout.service';
 export class GoogleSignInButtonComponent implements OnInit, OnDestroy {
   private destroy$ = new Subject<boolean>();
 
-  constructor(public modalService: NgbModal,
-              public userService: UserService,
-              public authService: SigninSignoutService,
-              public socialAuthService: SocialAuthService) {
+  constructor(public userService: UserService) {
   }
 
   ngOnInit(): void {
-    this.socialAuthService.authState.pipe(takeUntil(this.destroy$)).subscribe((googleUser: SocialUser) => {
-      const {email, idToken, authToken, authorizationCode} = googleUser;
-      this.authService.signInGoogleData({email, idToken, authToken, authorizationCode, type: 'google'})
-        .pipe(takeUntil(this.destroy$))
-        .subscribe(() => {
-          this.userService.fetchUserStatus();
-          this.modalService.dismissAll();
-        });
-    });
   }
 
   ngOnDestroy(): void {
