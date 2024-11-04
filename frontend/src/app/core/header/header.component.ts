e642d47c9339b9d36e84010a652e0fd048bffcf1
openminded-oscar <oleh.kosar94@gmail.com>
2024-11-05 00:04:24 +0200

    Fixbugsep2024/make realter functionality (#94)
    * Add responsive menu
    
    * Merge branch 'develop' into fixbugsep2024/make-realter-functionality
    
    * develop:
      Add responsive menu (#93)
    
    * Add header menu for mobile
    
    ---------
    
    Co-authored-by: Oleh <>

--- frontend/src/app/core/header/header.component.ts
+++ frontend/src/app/core/header/header.component.ts
@@ -5,27 +5,14 @@ import {UserService} from '../../app-services/user.service';
 import {User} from '../../app-models/user';
 import {Subject} from 'rxjs';
 
+
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit, OnDestroy {
-  private destroy$ = new Subject<boolean>();
-  public isAuthenticated: boolean;
-  public isAdmin: boolean;
-
-  public currentRoute: string;
-  public user: User;
-
-  public isActiveRoute(route: string): boolean {
-    return this.currentRoute?.endsWith(route);
-  }
-
-  public isDefaultRoute() {
-    return this.currentRoute === '/';
-  }
-
   constructor(public router: Router,
               public userService: UserService) {
     this.userService.user$.subscribe(user => {
@@ -37,6 +24,26 @@ export class HeaderComponent implements OnInit, OnDestroy {
         this.isAuthenticated = false;
       }
     });
+  }
+  private destroy$ = new Subject<boolean>();
+  public isAuthenticated: boolean;
+  public isAdmin: boolean;
+
+  public currentRoute: string;
+  public user: User;
+
+  public isMenuCollapsed = true;
+
+  public isActiveRoute(route: string): boolean {
+    return this.currentRoute?.endsWith(route);
+  }
+
+  public isDefaultRoute() {
+    return this.currentRoute === '/';
+  }
+
+  public closeMenu() {
+    this.isMenuCollapsed = true;
   }
 
   ngOnInit(): void {
