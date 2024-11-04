5960c64b957edbed1fe11f5c00eeca92ea9e3715
Oleh <>
2024-11-04 12:24:16 +0200

    Remove unused methods in repo

--- backend/src/main/java/co/oleh/realperfect/repository/RealtyObjectRepository.java
+++ backend/src/main/java/co/oleh/realperfect/repository/RealtyObjectRepository.java
@@ -1,16 +1,11 @@
 package co.oleh.realperfect.repository;
 
-import co.oleh.realperfect.model.OperationType;
 import co.oleh.realperfect.model.RealtyObject;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface RealtyObjectRepository extends JpaRepository<RealtyObject, Long>,
         JpaSpecificationExecutor<RealtyObject> {
-    List<RealtyObject> findRealtyObjectByTargetOperationsContaining(OperationType operationType);
-    List<RealtyObject> findByOwnerId(Long userId);
 }
