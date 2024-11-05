b1245c78ca1aa90b93f5373b2b00ce216f752717
Oleh <>
2024-11-06 00:33:42 +0200

    Add user email confirmation flow

--- backend/src/main/java/co/oleh/realperfect/auth/api/UserApi.java
+++ backend/src/main/java/co/oleh/realperfect/auth/api/UserApi.java
@@ -3,11 +3,14 @@ package co.oleh.realperfect.auth.api;
 import co.oleh.realperfect.auth.CredentialsValidator;
 import co.oleh.realperfect.auth.SpringSecurityUser;
 import co.oleh.realperfect.auth.UserService;
+import co.oleh.realperfect.emails.EmailSenderService;
 import co.oleh.realperfect.mapping.UserProfileDto;
 import co.oleh.realperfect.mapping.UserSelfDto;
+import co.oleh.realperfect.mapping.mappers.MappingService;
 import co.oleh.realperfect.model.user.EmailPasswordDto;
 import co.oleh.realperfect.model.user.User;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
@@ -17,31 +20,49 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping(value = "/api/user")
 @AllArgsConstructor
+@Slf4j
 public class UserApi {
-  private UserService userService;
+    private final MappingService mappingService;
+    private UserService userService;
+    private EmailSenderService emailSenderService;
+    private CredentialsValidator credentialsValidator;
 
-  private CredentialsValidator credentialsValidator;
+    @InitBinder("credentials")
+    protected void initBinder(WebDataBinder binder) {
+        binder.setValidator(credentialsValidator);
+    }
 
-  @InitBinder("credentials")
-  protected void initBinder(WebDataBinder binder) {
-    binder.setValidator(credentialsValidator);
-  }
+    @PostMapping
+    public UserSelfDto create(@Valid @RequestBody EmailPasswordDto credentials) {
+        User user = new User();
 
-  @PostMapping
-  public User create(@Valid @RequestBody EmailPasswordDto credentials) {
-    User user = new User();
+        user.setLogin(credentials.getEmail());
+        user.setEmail(credentials.getEmail());
+        user.setPassword(credentials.getPassword());
+        user.setUserConfirmed(false);
 
-    user.setLogin(credentials.getEmail());
-    user.setEmail(credentials.getEmail());
-    user.setPassword(credentials.getPassword());
-    userService.save(user);
+        userService.save(user);
+        try {
+            emailSenderService.sendEmailRegistrationConfirm(user.getEmail());
+        } catch (Exception e) {
+            log.error("ErrorWhileSendingUserAccountConfirmation letter {}. Details: {}", user.getEmail(), e.getMessage());
+        }
 
-    return user;
-  }
+        return this.mappingService.map(user, UserSelfDto.class);
+    }
 
-  @PatchMapping
-  public UserSelfDto updateMyProfile(@AuthenticationPrincipal SpringSecurityUser currentUser,
-                                     @Valid @RequestBody UserProfileDto user) {
-    return userService.patchProfile(currentUser.getId(), user);
-  }
+    @GetMapping("/confirm/{email}")
+    public UserSelfDto userEmailConfirmation(@PathVariable String email) {
+        User user = this.userService.findByLogin(email);
+
+        this.userService.saveUserIsConfirmed(user.getId());
+
+        return this.mappingService.map(user, UserSelfDto.class);
+    }
+
+    @PatchMapping
+    public UserSelfDto updateMyProfile(@AuthenticationPrincipal SpringSecurityUser currentUser,
+                                       @Valid @RequestBody UserProfileDto user) {
+        return userService.patchProfile(currentUser.getId(), user);
+    }
 }
---
+++ backend/src/main/java/co/oleh/realperfect/emails/EmailSenderService.java
@@ -0,0 +1,58 @@
+package co.oleh.realperfect.emails;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Service
+public class EmailSenderService {
+    @Value("${server.apiRoot}")
+    private String apiRoot;
+
+    private JavaMailSender emailSender;
+
+    public EmailSenderService(JavaMailSender emailSender) {
+        this.emailSender = emailSender;
+    }
+
+    public void sendEmailRegistrationConfirm(String to) throws MessagingException {
+        String confirmationLink = String.format("%s/api/user/confirm/%s", apiRoot, to);
+        String htmlContent = String.format("""
+        Congrats, you have successfully registered at The Best realty service!
+        <a href="%s">Click here to complete registration</a>
+        """, confirmationLink);
+
+        this.sendHtmlMessage(to,
+                "Registration Confirmation",
+                htmlContent
+        );
+    }
+
+    public void sendSimpleMessage(
+            String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("noreply@noreply.com");
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+
+        emailSender.send(message);
+    }
+
+    private void sendHtmlMessage(String to,
+                                 String subject,
+                                 String htmlBody) throws MessagingException {
+        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+        helper.setFrom("noreply@noreply.com");
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(htmlBody, true);
+        emailSender.send(message);
+    }
+}
