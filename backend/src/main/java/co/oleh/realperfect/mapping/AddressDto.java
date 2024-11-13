aa27cb2068137b51ba3e03124dcdc0afd765f0ab
Oleh <>
2024-11-13 21:17:35 +0200

    Add validation for Realty Object Address geolocation

--- backend/src/main/java/co/oleh/realperfect/mapping/AddressDto.java
+++ backend/src/main/java/co/oleh/realperfect/mapping/AddressDto.java
@@ -2,12 +2,19 @@ package co.oleh.realperfect.mapping;
 
 import lombok.Data;
 
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+
 @Data
 public class AddressDto {
     private String city;
     private String street;
     private String numberOfStreet;
     private Integer apartmentNumber;
+    @DecimalMin(value = "-180.0", inclusive = true, message = "Longitude must be between -180 and 180")
+    @DecimalMax(value = "180.0", inclusive = true, message = "Longitude must be between -180 and 180")
     private Double lng;
+    @DecimalMin(value = "-90.0", inclusive = true, message = "Latitude must be between -90 and 90")
+    @DecimalMax(value = "90.0", inclusive = true, message = "Latitude must be between -90 and 90")
     private Double lat;
 }
