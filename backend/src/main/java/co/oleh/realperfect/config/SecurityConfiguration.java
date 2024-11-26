b6f94606333119b8f736294a6708a7c9a1b99481
Oleh <>
2024-11-26 15:57:30 +0200

    Remove old security classes

--- backend/src/main/java/co/oleh/realperfect/config/SecurityConfiguration.java
+++ backend/src/main/java/co/oleh/realperfect/config/SecurityConfiguration.java
@@ -1,9 +1,6 @@
 package co.oleh.realperfect.config;
 
 import co.oleh.realperfect.auth.JWTAuthenticationFilter;
-import co.oleh.realperfect.config.oauth.CustomAuthorizationRequestRepository;
-import co.oleh.realperfect.config.oauth.Oauth2TokenSettingFilter;
-import co.oleh.realperfect.config.oauth.ScopeAwareOAuth2AuthorizationRequestResolver;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
@@ -15,11 +12,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationCodeTokenResponseClient;
-import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient;
-import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
-import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
-import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -31,16 +23,10 @@ import java.io.IOException;
 @EnableMethodSecurity(jsr250Enabled = true)
 public class SecurityConfiguration {
     private final JWTAuthenticationFilter jwtAuthenticationFilter;
-    private final ScopeAwareOAuth2AuthorizationRequestResolver oAuth2AuthorizationRequestResolver;
-    private final Oauth2TokenSettingFilter oauth2TokenSettingFilter;
 
-    public SecurityConfiguration(JWTAuthenticationFilter jwtAuthenticationFilter,
-                                 ScopeAwareOAuth2AuthorizationRequestResolver oAuth2AuthorizationRequestResolver,
-                                 Oauth2TokenSettingFilter oauth2TokenSettingFilter) {
+    public SecurityConfiguration(JWTAuthenticationFilter jwtAuthenticationFilter) {
         super();
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
-        this.oAuth2AuthorizationRequestResolver = oAuth2AuthorizationRequestResolver;
-        this.oauth2TokenSettingFilter = oauth2TokenSettingFilter;
     }
 
     @Bean
@@ -83,30 +69,9 @@ public class SecurityConfiguration {
                 // Exception handling
                 .exceptionHandling(exceptions -> exceptions
                         .authenticationEntryPoint(new RestAuthenticationEntryPoint())
-                )
-
-                // OAuth2 login configuration
-                .oauth2Login(oauth2 -> oauth2
-                        .authorizationEndpoint(endpoint -> endpoint
-                                .authorizationRequestRepository(authorizationRequestRepository())
-                                .authorizationRequestResolver(this.oAuth2AuthorizationRequestResolver)
-                        )
-                        .tokenEndpoint(token -> token
-                                .accessTokenResponseClient(accessTokenResponseClient())
-                        )
                 );
 
         return http.build();
-    }
-
-    @Bean
-    public OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest> accessTokenResponseClient() {
-        return new DefaultAuthorizationCodeTokenResponseClient();
-    }
-
-    @Bean
-    public AuthorizationRequestRepository<OAuth2AuthorizationRequest> authorizationRequestRepository() {
-        return new CustomAuthorizationRequestRepository();
     }
 
     public static class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
