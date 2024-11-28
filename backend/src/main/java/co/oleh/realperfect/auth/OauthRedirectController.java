8a19552994a75fe7ae4083579569a90a018baab9
Oleh <>
2024-11-28 14:50:06 +0200

    Add RateLimiter

--- backend/src/main/java/co/oleh/realperfect/auth/OauthRedirectController.java
+++ backend/src/main/java/co/oleh/realperfect/auth/OauthRedirectController.java
@@ -1,4 +1,4 @@
-package co.oleh.realperfect.commons;
+package co.oleh.realperfect.auth;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
