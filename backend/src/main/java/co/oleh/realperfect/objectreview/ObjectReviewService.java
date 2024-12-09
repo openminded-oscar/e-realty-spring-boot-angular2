77f492e5c09c958cd50c359e08af2ef365dcf94f
openminded-oscar <oleh.kosar94@gmail.com>
2024-12-09 17:10:18 +0200

    [TO NOT BE MERGED YET][TOBETESTED] Add ensure for parallel overlapping review couldn't be done (#148)
    * Add ensure for parallel overlapping review couldn't be done
    
    * Fix transactional package for Spring transaction
    
    ---------
    
    Co-authored-by: Oleh <>

--- backend/src/main/java/co/oleh/realperfect/objectreview/ObjectReviewService.java
+++ backend/src/main/java/co/oleh/realperfect/objectreview/ObjectReviewService.java
@@ -10,7 +10,9 @@ import co.oleh.realperfect.repository.ObjectReviewRepository;
 import co.oleh.realperfect.repository.RealtyObjectCrudRepository;
 import co.oleh.realperfect.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.Instant;
 import java.time.LocalTime;
@@ -26,6 +28,7 @@ import java.util.stream.Collectors;
 public class ObjectReviewService {
     public static final int OBJECT_REVIEW_START_HOUR = 10;
     public static final int OBJECT_REVIEW_END_HOUR = 20;
+    public static final int OBJECT_REVIEW_DURATION_MINUTES = 30;
 
     private final RealtyObjectCrudRepository realtyObjectRepository;
     private UserRepository userRepository;
@@ -33,6 +36,8 @@ public class ObjectReviewService {
     private MappingService mappingService;
 
     public MyObjectReviewDto save(ObjectReviewDto objectReview) {
+        verifyNoOverlappingReview(objectReview);
+
         ObjectReview objectReviewEntity = mappingService.map(objectReview, ObjectReview.class);
         RealtyObject realtyObject = realtyObjectRepository.findById(objectReview.getRealtyObjId()).get();
         User user = userRepository.findById(objectReview.getUserId()).get();
@@ -44,6 +49,16 @@ public class ObjectReviewService {
         ObjectReview savedEntity = objectReviewRepository.save(objectReviewEntity);
 
         return mappingService.map(savedEntity, MyObjectReviewDto.class);
+    }
+
+    private void verifyNoOverlappingReview(ObjectReviewDto objectReview) {
+//      Next step is using @Lock(LockModeType.PESSIMISTIC_WRITE) in repo level (do we need it???)
+        Instant beginTime = objectReview.getDateTime();
+        Instant endTime = beginTime.plus(OBJECT_REVIEW_DURATION_MINUTES, ChronoUnit.MINUTES);
+        List<ObjectReview> overlappingReviews = objectReviewRepository.findByRealtyObjIdAndDateTimeBetween(objectReview.getRealtyObjId(), beginTime, endTime);
+        if (!overlappingReviews.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Object review already exists");
+        }
     }
 
     public List<ObjectReview> remove(List<ObjectReview> objectReviews) {
