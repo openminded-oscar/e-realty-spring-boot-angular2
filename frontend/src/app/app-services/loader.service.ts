4c205ff7a59d05ac909b1e6e08da7725ceb5f846
Oleh <>
2024-12-12 16:01:33 +0200

    [Angular] Fix expressionChanged bug

--- frontend/src/app/app-services/loader.service.ts
+++ frontend/src/app/app-services/loader.service.ts
@@ -1,27 +1,30 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import {ChangeDetectorRef, Injectable} from '@angular/core';
+import {BehaviorSubject} from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class LoaderService {
-  private requestCount = 0;
-  private isLoading = new BehaviorSubject<boolean>(false);
-  public isLoading$ = this.isLoading.asObservable();
+    private requestCount = 0;
+    private isLoading = new BehaviorSubject<boolean>(false);
+    public isLoading$ = this.isLoading.asObservable();
 
-  public show() {
-    this.requestCount++;
-    this.updateLoadingState();
-  }
-
-  public hide() {
-    if (this.requestCount > 0) {
-      this.requestCount--;
+    constructor() {
     }
-    this.updateLoadingState();
-  }
 
-  private updateLoadingState() {
-    this.isLoading.next(this.requestCount > 0);
-  }
+    public show() {
+        this.requestCount++;
+        this.updateLoadingState();
+    }
+
+    public hide() {
+        if (this.requestCount > 0) {
+            this.requestCount--;
+        }
+        this.updateLoadingState();
+    }
+
+    private updateLoadingState() {
+        this.isLoading.next(this.requestCount > 0);
+    }
 }
