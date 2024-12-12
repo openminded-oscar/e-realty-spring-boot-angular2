986a399ce59ca2e081871b00408721bb63b1bba6
openminded-oscar <oleh.kosar94@gmail.com>
2024-12-12 15:33:19 +0200

    Add rate limiting for file uploading

--- backend/src/main/java/co/oleh/realperfect/pictures/FileUploadController.java
+++ backend/src/main/java/co/oleh/realperfect/pictures/FileUploadController.java
@@ -4,17 +4,20 @@ import co.oleh.realperfect.model.photos.ConfirmationDocPhoto;
 import co.oleh.realperfect.model.photos.Photo;
 import co.oleh.realperfect.model.photos.RealtyObjectPhoto;
 import co.oleh.realperfect.model.photos.UserPhoto;
+import co.oleh.realperfect.ratelimiter.RateLimited;
 import lombok.AllArgsConstructor;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -27,6 +30,8 @@ public class FileUploadController {
 
 
     @PostMapping(value = "/upload-photo/{type}", produces = "application/json")
+    @PreAuthorize("isAuthenticated()")
+    @RateLimited(requestsPerHour = 200)
     public ResponseEntity<Photo> handleFileUpload(@RequestParam("file") MultipartFile file, @PathVariable String type) {
         String filename = generateUuidFilename(file);
         storageService.uploadFileForCategoryAndUser(file, filename, "category", "someId");
@@ -42,10 +47,9 @@ public class FileUploadController {
     }
 
     private String generateUuidFilename(MultipartFile file) {
-        String[] filenameParts = file.getOriginalFilename().split("\\.");
-        String filename = UUID.randomUUID() + "." + filenameParts[filenameParts.length - 1];
+        String[] filenameParts = Objects.requireNonNull(file.getOriginalFilename()).split("\\.");
 
-        return filename;
+        return UUID.randomUUID() + "." + filenameParts[filenameParts.length - 1];
     }
 
     @GetMapping("/list-uploaded-photos")
