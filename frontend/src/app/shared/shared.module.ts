d68c992164e97c27cd0c9a5c5ac73e0eb273eb71
Oleh <>
2024-12-13 11:48:08 +0200

    Add basic filter function

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -2,68 +2,75 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
 import {
-  NgbCollapseModule,
-  NgbDatepickerModule,
-  NgbDropdownModule,
-  NgbModalModule,
-  NgbToastModule, NgbTooltipModule,
+    NgbCollapseModule,
+    NgbDatepickerModule,
+    NgbDropdownModule,
+    NgbModalModule,
+    NgbToastModule,
+    NgbTooltipModule,
 } from '@ng-bootstrap/ng-bootstrap';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {RealtyObjsListComponent} from './realty-objs-list/realty-objs-list.component';
 import {DeleteRealtyModalComponent} from './delete-realty-modal/delete-realty-modal.component';
 import {ScheduleFormModalComponent} from './schedule-form-modal/schedule-form-modal.component';
 import {ConfirmModalComponent} from './confirm-modal/confirm-modal.component';
-import {RealtyObjCardDetailsComponent} from './realty-obj-card/realty-obj-card-details/realty-obj-card-details.component';
+import {
+    RealtyObjCardDetailsComponent
+} from './realty-obj-card/realty-obj-card-details/realty-obj-card-details.component';
 import {RealtorContactComponent} from './realtor-contact/realtor-contact.component';
-import { RealtyStatusComponent } from './realty-status/realty-status.component';
-import { MessageModalComponent } from './message-modal/message-modal.component';
+import {RealtyStatusComponent} from './realty-status/realty-status.component';
+import {MessageModalComponent} from './message-modal/message-modal.component';
 import {ReviewsListComponent} from '../user-cabinet/reviews/app-reviews-list/reviews-list.component';
 import {ReviewsFilterComponent} from '../user-cabinet/reviews/app-reviews-filter/reviews-filter.component';
 import {RealtyObjCardComponent} from './realty-obj-card/realty-obj-card.component';
 import {RealtyObjReviewCardComponent} from './realty-obj-card/realty-obj-review-card.component';
 import {CancelReviewModalComponent} from './cancel-review-modal/cancel-review-modal.component';
+import {
+    ObjectsInProfileFilterComponent
+} from '../user-cabinet/user-objects/objects-in-profile-filter/objects-in-profile-filter.component';
 
 const components = [
-  RealtorContactComponent,
-  ReviewsListComponent,
-  ReviewsFilterComponent,
-  RealtyObjsListComponent,
-  RealtyObjCardDetailsComponent,
-  RealtyObjCardComponent,
-  RealtyObjReviewCardComponent,
-  DeleteRealtyModalComponent,
-  ScheduleFormModalComponent,
-  ConfirmModalComponent,
-  CancelReviewModalComponent,
-  MessageModalComponent,
-  RealtyStatusComponent
+    ObjectsInProfileFilterComponent,
+    RealtorContactComponent,
+    ReviewsListComponent,
+    ReviewsFilterComponent,
+    RealtyObjsListComponent,
+    RealtyObjCardDetailsComponent,
+    RealtyObjCardComponent,
+    RealtyObjReviewCardComponent,
+    DeleteRealtyModalComponent,
+    ScheduleFormModalComponent,
+    ConfirmModalComponent,
+    CancelReviewModalComponent,
+    MessageModalComponent,
+    RealtyStatusComponent
 ];
 
 @NgModule({
-  declarations: [...components],
-  exports: [
-    ...components,
-    NgbModalModule,
-    NgbDatepickerModule,
-    NgbDropdownModule,
-    NgbTooltipModule,
-    NgbToastModule,
-    NgbCollapseModule,
-    FormsModule,
-    ReactiveFormsModule
-  ],
-  imports: [
-    CommonModule,
-    RouterModule,
-    NgbModalModule,
-    NgbDatepickerModule,
-    NgbDropdownModule,
-    NgbToastModule,
-    NgbTooltipModule,
-    NgbCollapseModule,
-    FormsModule,
-    ReactiveFormsModule
-  ]
+    declarations: [...components],
+    exports: [
+        ...components,
+        NgbModalModule,
+        NgbDatepickerModule,
+        NgbDropdownModule,
+        NgbTooltipModule,
+        NgbToastModule,
+        NgbCollapseModule,
+        FormsModule,
+        ReactiveFormsModule
+    ],
+    imports: [
+        CommonModule,
+        RouterModule,
+        NgbModalModule,
+        NgbDatepickerModule,
+        NgbDropdownModule,
+        NgbToastModule,
+        NgbTooltipModule,
+        NgbCollapseModule,
+        FormsModule,
+        ReactiveFormsModule
+    ]
 })
 export class SharedModule {
 }
--- frontend/src/app/user-cabinet/realtor-objects/realtor-objects.component.ts
+++ frontend/src/app/user-cabinet/realtor-objects/realtor-objects.component.ts
@@ -2,35 +2,44 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Subject} from 'rxjs';
 import {takeUntil} from 'rxjs/operators';
 import {RealtorService} from '../../app-services/realtor.service';
-import {RealtyObj} from '../../app-models/realty-obj';
+import {filterByObjectStatus, RealtyObj, RealtyObjectByStatusFilter} from '../../app-models/realty-obj';
+
 
 @Component({
-  selector: 'app-realtor-objects',
-  templateUrl: './realtor-objects.component.html',
-  styleUrls: ['./realtor-objects.component.scss']
+    selector: 'app-realtor-objects',
+    templateUrl: './realtor-objects.component.html',
+    styleUrls: ['./realtor-objects.component.scss']
 })
 export class RealtorObjectsComponent implements OnInit, OnDestroy {
-  public destroy$ = new Subject<boolean>();
-  public realtorObjects: RealtyObj[];
-  public filter: 'all' | 'active' | 'archived' | 'drafts' = 'all';
+    public destroy$ = new Subject<boolean>();
+    public allRealtorObjects: RealtyObj[] = [];
+    public filter: RealtyObjectByStatusFilter = 'all';
+    public filteredObjects: RealtyObj[] = [];
 
-  constructor(public realtorService: RealtorService) {
-  }
+    constructor(public realtorService: RealtorService) {
+    }
 
-  ngOnInit(): void {
-    this.realtorService.getMyAsRealtorObjects()
-      .pipe(takeUntil(this.destroy$))
-      .subscribe(objects => {
-        this.realtorObjects = objects;
-      });
-  }
+    ngOnInit(): void {
+        this.realtorService.getMyAsRealtorObjects()
+            .pipe(takeUntil(this.destroy$))
+            .subscribe(objects => {
+                this.allRealtorObjects = objects;
+                this.applyFilter();
+            });
+    }
 
-  ngOnDestroy(): void {
-    this.destroy$.next(true);
-    this.destroy$.complete();
-  }
+    ngOnDestroy(): void {
+        this.destroy$.next(true);
+        this.destroy$.complete();
+    }
 
-  public setFilter(filterSelected: 'all' | 'active' | 'archived' | 'drafts') {
-    this.filter = filterSelected;
-  }
+    public applyFilter(): void {
+        this.filteredObjects = filterByObjectStatus(this.allRealtorObjects, this.filter)
+        debugger
+    }
+
+    public setFilter(filterSelected: RealtyObjectByStatusFilter) {
+        this.filter = filterSelected;
+        this.applyFilter();
+    }
 }
