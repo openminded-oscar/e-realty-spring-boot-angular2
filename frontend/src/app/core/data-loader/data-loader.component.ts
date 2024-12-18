e42a0d622bf2af7090e4e68bfa02df4a3deb067a
openminded-oscar <oleh.kosar94@gmail.com>
2024-12-18 12:01:03 +0200

    Fixbugsep2024/fix loader blinking (#166)
    * Move common logic to service
    
    * Merge branch 'develop' into fixbugsep2024/december-changes
    
    * develop:
      Fixbugsep2024/fix remove review code structurev3 (#165)
      Fixbugsep2024/fix remove review code structurev3 (#164)
    
    # Conflicts:
    #	frontend/src/app/app-services/reviews.service.ts
    #	frontend/src/app/home/realty-obj-details/realty-obj-details.component.ts
    #	frontend/src/app/shared/approve-review-modal/approve-review-modal.component.ts
    #	frontend/src/app/shared/schedule-form-modal/schedule-form-modal.component.ts
    
    ---------
    
    Co-authored-by: Oleh <>

--- frontend/src/app/core/data-loader/data-loader.component.ts
+++ frontend/src/app/core/data-loader/data-loader.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+export const MIN_LOADER_SHOW_TIME = 300;
+
 @Component({
   selector: 'app-data-loader',
   templateUrl: './data-loader.component.html',
