a983fdbeeee955086fad32fec0377df07433d981
Oleh <>
2025-02-01 23:10:28 +0200

    Fix initial data test

--- backend/local/data.sql
+++ backend/local/data.sql
@@ -1,14 +1,14 @@
 INSERT INTO `tbl_role` (`id`, `name`) VALUES
-                                          (2000, 'Admin'),
-                                          (2001, 'User'),
-                                          (2002, 'Realtor');
+                                          (2000, 'ADMIN'),
+                                          (2001, 'USER'),
+                                          (2002, 'REALTOR');
 
 INSERT INTO `tbl_user` (
-    `id`, `email`, `google_user_id_token_subject`, `login`, `name`, `password`,
+    `id`, `email`, `google_user_id_token_subject`, `login`, `name`, `password`, `is_user_confirmed`,
     `phone_number`, `profile_pic`, `surname`, `created_at`, `updated_at`
 ) VALUES
-      (2001, 'admin@gmail.com', 'abc123', 'user1', 'John', 'password1', '1234567890', 'profile_pic1.jpg', 'Doe', '2024-10-04 21:28:48.102443', '2024-10-04 21:28:48.102443'),
-      (2002, 'user@gmail.com', 'def456', 'user2', 'Jane', 'password2', '0987654321', 'profile_pic2.jpg', 'Smith', '2024-10-04 21:28:48.102443', '2024-10-04 21:28:48.102443');
+      (2001, 'admin@gmail.com', 'abc123', 'user1', 'John', 'password1', true,'1234567890', 'profile_pic1.jpg', 'Doe', '2024-10-04 21:28:48.102443', '2024-10-04 21:28:48.102443'),
+      (2002, 'user@gmail.com', 'def456', 'user2', 'Jane', 'password2', true, '0987654321', 'profile_pic2.jpg', 'Smith', '2024-10-04 21:28:48.102443', '2024-10-04 21:28:48.102443');
 
 
 INSERT INTO `tbl_user_role` (`user_id`, `role_id`) VALUES
