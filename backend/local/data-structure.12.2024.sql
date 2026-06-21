72ddd0bc253a53891a7667c9fe88d56e01e1212c
Oleh <>
2026-06-21 13:21:56 +0300

    [master] apply changes to tables user, realtor, realty object - according to the codebase

--- backend/local/data-structure.12.2024.sql
+++ backend/local/data-structure.12.2024.sql
@@ -1,43 +1,39 @@
+-- realperfect.tbl_user_photo definition
 CREATE TABLE `tbl_user_photo` (
                                   `id` bigint NOT NULL AUTO_INCREMENT,
                                   `filename` varchar(255) DEFAULT NULL,
                                   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+-- realperfect.tbl_user definition
 CREATE TABLE `tbl_user` (
                             `id` bigint NOT NULL AUTO_INCREMENT,
+                            `created_at` datetime(6) DEFAULT NULL,
+                            `updated_at` datetime(6) DEFAULT NULL,
+                            `email` varchar(255) NOT NULL,
                             `google_user_id_token_subject` varchar(255) DEFAULT NULL,
                             `login` varchar(255) DEFAULT NULL,
                             `name` varchar(255) DEFAULT NULL,
                             `password` varchar(255) DEFAULT NULL,
                             `phone_number` varchar(255) DEFAULT NULL,
                             `surname` varchar(255) DEFAULT NULL,
-                            `realtor_id` bigint DEFAULT NULL,
-                            `created_at` datetime(6) DEFAULT NULL,
-                            `updated_at` datetime(6) DEFAULT NULL,
-                            `photo_id` bigint DEFAULT NULL,
-                            `profile_pic` varchar(255) DEFAULT NULL,
-                            `email` varchar(255) NOT NULL,
                             `is_user_confirmed` bit(1) NOT NULL,
+                            `photo_id` bigint DEFAULT NULL,
                             PRIMARY KEY (`id`),
-                            KEY `FKecos2jf3rywyoo11l4qigm403` (`photo_id`),
-                            KEY `FK9ia3vn2v05ga54wxnpjnh0sye` (`realtor_id`),
+                            UNIQUE KEY `UK_npn1wf1yu1g5rjohbek375pp1` (`email`),
+                            UNIQUE KEY `UK_9v8o3s7mlfsovphoygcl5vlu1` (`photo_id`),
                             CONSTRAINT `FKecos2jf3rywyoo11l4qigm403` FOREIGN KEY (`photo_id`) REFERENCES `tbl_user_photo` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2042 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+-- realperfect.tbl_realtor definition
 CREATE TABLE `tbl_realtor` (
                                `id` bigint NOT NULL AUTO_INCREMENT,
                                `user_id` bigint DEFAULT NULL,
-                               `name` varchar(255) DEFAULT NULL,
-                               `surname` varchar(255) DEFAULT NULL,
                                PRIMARY KEY (`id`),
+                               UNIQUE KEY `UK_dny6cjrq8udgvmetluuf97o9b` (`user_id`),
                                KEY `idx_user_id` (`user_id`),
                                CONSTRAINT `FKsskttedvoj27m8dkawlrc1dmq` FOREIGN KEY (`user_id`) REFERENCES `tbl_user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-ALTER TABLE `tbl_user`
-    ADD CONSTRAINT `FK9ia3vn2v05ga54wxnpjnh0sye` FOREIGN KEY (`realtor_id`) REFERENCES `tbl_realtor` (`id`);
-
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `email_confirmation_token` (
                                             `id` bigint NOT NULL AUTO_INCREMENT,
@@ -57,15 +53,23 @@ CREATE TABLE `tbl_confirmation_doc_photo` (
                                               PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+-- realperfect.tbl_realty_object definition
 CREATE TABLE `tbl_realty_object` (
                                      `id` bigint NOT NULL AUTO_INCREMENT,
+                                     `created_at` datetime(6) DEFAULT NULL,
+                                     `updated_at` datetime(6) DEFAULT NULL,
                                      `apt_number` int DEFAULT NULL,
                                      `city` varchar(255) DEFAULT NULL,
+                                     `geolocation` geometry SRID 4326 DEFAULT NULL,
                                      `number_of_street` varchar(255) DEFAULT NULL,
+                                     `region_id` bigint DEFAULT NULL,
+                                     `region_lat` double DEFAULT NULL,
+                                     `region_lng` double DEFAULT NULL,
+                                     `region_name` varchar(255) DEFAULT NULL,
                                      `street` varchar(255) DEFAULT NULL,
-                                     `building_type` varchar(255) DEFAULT NULL,
+                                     `building_type` enum('BLOCK','BRICK','WOODEN') DEFAULT NULL,
                                      `description` varchar(255) DEFAULT NULL,
-                                     `dwelling_type` varchar(255) DEFAULT NULL,
+                                     `dwelling_type` enum('APARTMENT','HOUSE') DEFAULT NULL,
                                      `floor` int DEFAULT NULL,
                                      `foundation_year` int DEFAULT NULL,
                                      `has_cellar` bit(1) DEFAULT NULL,
@@ -75,33 +79,25 @@ CREATE TABLE `tbl_realty_object` (
                                      `living_area` decimal(38,2) DEFAULT NULL,
                                      `other_info` varchar(255) DEFAULT NULL,
                                      `price` decimal(38,2) DEFAULT NULL,
-                                     `realtor_aware` bit(1) DEFAULT NULL,
+                                     `price_for_rent` decimal(38,2) DEFAULT NULL,
                                      `rooms_amount` int DEFAULT NULL,
+                                     `status` enum('ACTIVE','ARCHIVED','DRAFT') DEFAULT NULL,
                                      `total_area` decimal(38,2) DEFAULT NULL,
                                      `total_floors` int DEFAULT NULL,
                                      `realty_object_id` bigint DEFAULT NULL,
                                      `owner_id` bigint DEFAULT NULL,
                                      `realtor_id` bigint DEFAULT NULL,
-                                     `created_at` datetime(6) DEFAULT NULL,
-                                     `updated_at` datetime(6) DEFAULT NULL,
-                                     `price_for_rent` decimal(38,2) DEFAULT NULL,
-                                     `status` varchar(255) DEFAULT NULL,
-                                     `geolocation` geometry SRID 4326 NOT NULL,
-                                     `region_id` bigint DEFAULT NULL,
-                                     `region_lat` double DEFAULT NULL,
-                                     `region_lng` double DEFAULT NULL,
-                                     `region_name` varchar(255) DEFAULT NULL,
                                      PRIMARY KEY (`id`),
-                                     KEY `idx_region` (`region_id`),
+                                     UNIQUE KEY `UK_4bmwhw2n7ugmgk2jhxnvibs9u` (`realty_object_id`),
                                      KEY `idx_city` (`city`),
+                                     KEY `idx_region` (`region_id`),
                                      KEY `idx_owner_id` (`owner_id`),
                                      KEY `idx_realtor_id` (`realtor_id`),
-                                     SPATIAL KEY `idx_geolocation` (`geolocation`),
-                                     KEY `FK7ujvwekhjj7mip1e53tmlwtjc` (`realty_object_id`),
-                                     CONSTRAINT `FK4puv7o3577yfw40g3y06ta5e9` FOREIGN KEY (`realtor_id`) REFERENCES `tbl_realtor` (`id`),
+                                     CONSTRAINT `FK6wo525po30ofjdeqemfslhjyc` FOREIGN KEY (`realtor_id`) REFERENCES `tbl_realtor` (`id`),
                                      CONSTRAINT `FK7ujvwekhjj7mip1e53tmlwtjc` FOREIGN KEY (`realty_object_id`) REFERENCES `tbl_confirmation_doc_photo` (`id`) ON DELETE SET NULL ON UPDATE RESTRICT,
                                      CONSTRAINT `FKghvmecgfl56awhev42qtql1cd` FOREIGN KEY (`owner_id`) REFERENCES `tbl_user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1066 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+ALTER TABLE tbl_realty_object ADD SPATIAL INDEX idx_geolocation (geolocation);
 
 CREATE TABLE `tbl_interest` (
                                 `id` bigint NOT NULL AUTO_INCREMENT,
@@ -127,8 +123,6 @@ CREATE TABLE `tbl_object_review` (
                                      `approved` bit(1) DEFAULT NULL,
                                      PRIMARY KEY (`id`),
                                      UNIQUE KEY `UKg44xh1vkknhcow0tjx798o149` (`realty_obj_id`,`date_time`),
-                                     UNIQUE KEY `UKdwe3aopxekixluj8mfinfy5rh` (`realty_obj_id`,`date_time`),
-                                     UNIQUE KEY `UKdvohjt5g6ndhbtjvyo8qb107m` (`realty_obj_id`,`date_time`),
                                      KEY `idx_realtor` (`realtor_id`),
                                      KEY `idx_user` (`user_id`),
                                      KEY `idx_realtyObj` (`realty_obj_id`),
